a0d3feca065c21b1e24a2c9604e763b94bb29680
Joseph Macam <jdmacam@gmail.com>
2020-04-23 19:20:32 -0700

    sort testing fix 1

--- Practice.cpp
+++ Practice.cpp
@@ -38,14 +38,6 @@ void Practice::sortDescending(int & first, int & second, int & third)
 // character in the string, but disregarding case ('x' is the same as 'X')
 bool Practice::isPalindrome(string input)
 {
-  for(int i=0; i < input.size(); i++)
-  {
-    if( input[i] < 'A' || input[i] > 'Z' )
-    {
-      //change lower case to upper case
-      input[i] = input[i] - ('a' - 'A');
-    }
-  }
   for(int i=0; i < input.size()/2; i++)
   {
     if( input[i] != input[input.size()-1-i] )
