f6ebfe2a12b822e19a6a8f1ec861b4adf8852250
Joseph Macam <jdmacam@gmail.com>
2020-04-23 18:21:04 -0700

    Add second unit test

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -19,4 +19,8 @@ TEST(PracticeTest, is_simple_palindrome)
     Practice obj;
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
+
+		actual = obj.isPalindrome("Ba");
+    ASSERT_TRUE(actual);
+
 }
